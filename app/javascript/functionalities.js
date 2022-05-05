d8bbd2634d7e12cb1cb8e533646a499d9b043b1f
Frcomparan <frcomparan@gmail.com>
2022-05-05 13:01:13 -0500

    Avoid see multiple photos on upload

--- app/javascript/functionalities.js
+++ app/javascript/functionalities.js
@@ -7,10 +7,24 @@ const showUserMenu = () => {
 }
 
 const loadFile = (event) => {
-	let image = document.getElementById('output');
+	let img = document.getElementById('output');
   document.getElementById('add').style.display = 'none';
-	image.src = URL.createObjectURL(event.target.files[0]);
-  image.style.display = 'block'
+	img.src = URL.createObjectURL(event.target.files[0]);
+  img.style.display = 'block'
+};
+
+const loadFiles = (event) => {
+	let container = document.getElementById('upload');
+  document.getElementById('add').style.display = 'none';
+  let i = 0;
+  const uploadPhotos = event.target.files;
+  for (let key = 0; key < uploadPhotos['length']; key++) {
+    img = '<img class="photo" id="output' + key + '"/>';
+    container.innerHTML = container.innerHTML + img;
+    img = document.getElementById('output' + key)
+    img.src = URL.createObjectURL(uploadPhotos[key]);
+    img.style.display = 'block'
+  }
 };
 
 window.onclick = function(event) {
@@ -19,7 +33,6 @@ window.onclick = function(event) {
   if (!(user_validation || filter_validation)) {
     let dropdowns = document.getElementsByClassName("menu");
     let i;
-    console.log(dropdowns)
     for (i = 0; i < dropdowns.length; i++) {
       let openDropdown = dropdowns[i];
       if (openDropdown.classList.contains('show')) {
